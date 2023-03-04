335628c213ac7c59af4dfe0acaf3bc3b100cc873
killshotxd <hassanansari211@gmail.com>
2023-03-04 09:56:11 +0530

    fixed timestamp

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { UserAuth } from "../context/AuthContext";
 
+import moment from "moment";
 const Message = ({ msg }) => {
+  const timestamp = moment(msg.createdAt.toDate());
+  const relativeTime = timestamp.fromNow();
+  console.log(relativeTime);
   const { currentUser } = UserAuth();
 
   return (
@@ -16,9 +20,9 @@ const Message = ({ msg }) => {
             <img src={msg.avatar} />
           </div>
         </div>
-        <div className="chat-header">
+        <div className="chat-header gap-1 flex items-center">
           {msg.name}
-          {/* <time className="text-xs opacity-50">12:45</time> */}
+          <time className="text-xs opacity-50">{relativeTime}</time>
         </div>
         <div className="chat-bubble">{msg.text}</div>
         <div className="chat-footer opacity-50">Delivered</div>
--- src/components/SendMsg.jsx
+++ src/components/SendMsg.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { UserAuth } from "../context/AuthContext";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../Firebase";
-
+import { BiSend } from "react-icons/bi";
 const SendMsg = () => {
   const [value, setValue] = useState("");
   const { currentUser } = UserAuth();
@@ -35,8 +35,8 @@ const SendMsg = () => {
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
-        <button className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">
-          Send
+        <button className="w-auto flex bg-gray-500 text-white rounded-r-lg px-5 py-2 gap-2 items-center justify-center m-0 text-sm">
+          Send <BiSend />
         </button>
       </form>
     </div>
